14c573617d7c7457d0b3e42c9b63b1bf9350ba27
SarBajit07 <104916170+SarBajit07@users.noreply.github.com>
2026-06-14 19:59:06 +0545

    first commit

---
+++ frontend/src/app/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css"
+
+function App() {
+    return (
+          <main
+          className=" h-screen w-full bg-gray-950 "
+          >
+            <aside 
+              className="h-full w-2/5 bg amber-50 "
+            ></aside>
+             
+          </main>
+    )
+}
+export default App
+
